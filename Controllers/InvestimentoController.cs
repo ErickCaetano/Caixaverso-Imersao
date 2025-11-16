144d922cececad2cd39fbb6120085f25a18f9069
Erick Caetano <erickcaetano@live.com>
2025-11-16 12:50:23 -0300

    Implementa enpoint Recomendação de investimento e endpoint investimentos por cliente.

--- Controllers/InvestimentoController.cs
+++ Controllers/InvestimentoController.cs
@@ -19,7 +19,6 @@ public class InvestimentoController : ControllerBase
 
 
     //1. Solicitação de Simulação de Investimento
-
     //POST: api/simular-investimento
 
     [HttpPost("simular-investimento")]
@@ -122,10 +121,64 @@ public class InvestimentoController : ControllerBase
         var investidor = DataBase.Investidores.Find(i => i.IdCliente == clienteId);
 
         if (investidor == null)
-            return NotFound();
+            return NotFound("Investidor não encontrado.");
         else
             return Ok(investidor);
     }
+
+
+    //6. Produtos Recomendados
+    //    GET:api/produtos-recomendados/{perfil}
+        [HttpGet("produtos-recomendados/{perfil}")]
+    public IActionResult RecomendacaoPerfil(string perfil)
+    {
+        if (string.IsNullOrWhiteSpace(perfil))
+            return BadRequest("Perfil investidor inválido.");
+        if (perfil != "Conservador" && perfil != "Moderado" && perfil != "Agressivo")
+            return BadRequest("Perfil investidor inválido.");
+
+
+        
+        string riscoProduto = perfil == "Conservador" ? "Baixo" : perfil == "Moderado" ? "Médio" : "Alto";
+
+        var response = DataBase.Produtos.FindAll(i => i.Risco == riscoProduto);
+        if (response == null || response.Count == 0)
+            return NotFound();
+        else
+            return Ok(response);
+    }
+
+
+
+    //7. Histórico de Investimentos
+    //    GET:api/investimentos/{clienteId}
+
+    [HttpGet("investimentos/{clienteId}")]
+    public IActionResult ListarInvestimentosPorCliente(int clienteId)
+    {   
+        
+        if (!DataBase.Investidores.Any(i => i.IdCliente == clienteId))
+        {
+            return NotFound("Investidor não encontrado.");
+        }
+        var investimentosCliente = DataBase.Simulacoes.Where(s => s.IdCliente == clienteId).ToList();
+
+        if (investimentosCliente == null || investimentosCliente.Count == 0)
+            return NotFound("Nenhum investimento encontrado para o investidor especificado.");
+
+        var response = investimentosCliente.Select(s => new
+        {
+            id = s.IdSimulacao,
+            tipo = s.Produto.Tipo,
+            valor = s.ValorInvestido,
+            rentabilidade = s.VerRentabilidade(),
+            data = s.DataSimulacao.ToString("yyyy-MM-dd")
+        });
+
+        return Ok(response);
+ 
+    }
+
 
 
 
